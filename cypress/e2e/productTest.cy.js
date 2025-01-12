c060708cea5f5799bca826bc699de9945d16ce62
Dag <adjeidag@gmail.com>
2025-01-12 12:19:30 +0000

    added a search product test

--- cypress/e2e/productTest.cy.js
+++ cypress/e2e/productTest.cy.js
@@ -23,9 +23,30 @@ describe('Product page tests', () => {
         cy.contains('Products').click()
         cy.url().should('include', '/products')
         cy.contains('h2', 'All Products')
-        cy.get('#search_product').type(faker.commerce.productName())
+        const searchItem = Math.random() > 0.5 ? faker.commerce.productName() : 'dress'
+        cy.get('#search_product').type(searchItem)
         cy.get('button[id="submit_search"]').click()
-        cy.contains('h2', 'Searched Products')
+        cy.contains('h2', 'Searched Products').should('be.visible')
+        cy.get('body').then(($body) => {
+            if ($body.find('.features_items').length > 0) {
+              cy.get('.features_items').then(($grid) => {
+                const products = $grid.find('.col-sm-4').length
+                if(products > 0){
+                    cy.get('.col-sm-4').should('be.visible')
+                }
+                else{
+                    cy.get('.features_items').find('.col-sm-4').should('have.length', 0)
+                }
+              }) 
+            }
+            else{
+              cy.get('.features_items').should('be.empty')
+            }
+        })
       })
+
+    it('Add products to Cart', () => {
+
+    })
   })
   
