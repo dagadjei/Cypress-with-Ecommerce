b927383c2f4c99f62586827862763fb0b5528c8d
Dag <adjeidag@gmail.com>
2025-02-24 19:01:51 +0000

    added all API tests for login

--- cypress/e2e/apiTests.cy.js
+++ cypress/e2e/apiTests.cy.js
@@ -1,5 +1,6 @@
-import { fa } from "@faker-js/faker";
+import { fa, faker } from "@faker-js/faker";
 import { parseResponse } from "../support/helpers";
+import  user  from "../fixtures/user.json";
 
 //There might be a general issue with the API as unsupported methods are returning 200 status code
 
@@ -91,6 +92,76 @@ describe('API Tests', () => {
         })
     })
 
+    it('POST To Verify Login with valid details', () => {
+        const email = user.email
+        const password = user.password
 
+        cy.request({
+            method: 'POST',
+            url: 'https://automationexercise.com/api/verifyLogin',
+            body: {
+                email: email,
+                password: password
+            }
+        }).then((response) => {
+            const responseBody = parseResponse(response)
+            const responseCode = responseBody.responseCode
+            /*
+            These do not work because of issues with the API
+            expect(responseCode).to.equal(200)
+            expect(responseBody).to.have.property('message', 'User exists!')
+            */
+        })
+    })
+
+    it('POST To Verify Login without email parameter', () => {
+        const password = user.password
+
+        cy.request({
+            method: 'POST',
+            url: ' https://automationexercise.com/api/verifyLogin',
+            body: {
+                password: password
+            },
+        }).then((response) => {
+            const responseBody = parseResponse(response)
+            const responseCode = responseBody.responseCode
+            expect(responseCode).to.equal(400)
+            expect(responseBody).to.have.property('message', 'Bad request, email or password parameter is missing in POST request.')
+        })
+    })
+
+    it('DELETE To Verify Login', () => {
+        cy.request({
+            method: 'DELETE',
+            url: 'https://automationexercise.com/api/verifyLogin',
+            failOnStatusCode: false,
+        }).then((response) => {
+            const responseBody = parseResponse(response)
+            const responseCode = responseBody.responseCode
+            expect(responseCode).to.equal(405)
+            expect(responseBody).to.have.property('message', 'This request method is not supported.')
+        })
+    })
+
+    it('POST To Verify Login with invalid details', () => {
+        const email = faker.internet.email()
+        const password = faker.internet.password()
+
+        cy.request({
+            method: 'POST',
+            url: 'https://automationexercise.com/api/verifyLogin',
+            body: {
+                email: email,
+                password: password
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            const responseBody = parseResponse(response)
+            const responseCode = responseBody.responseCode
+            //expect(responseCode).to.equal(404)
+            //expect(responseBody).to.have.property('message', 'User not found!')
+        })
+    })
 
 });
